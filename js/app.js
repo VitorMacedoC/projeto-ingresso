8e98ad8bc0a207352be4db2bd064db5aa762c883
Vitor Macedo <macedov836@gmai.com>
2025-04-14 21:50:18 -0300

    Criando funçoes para os outros tipos de ingressos

--- js/app.js
+++ js/app.js
@@ -2,19 +2,47 @@ function comprar() {
     let tipoIngresso = document.getElementById('tipo-ingresso');
     let quantidade = parseInt(document.getElementById('qtd').value);
 
-    if(tipoIngresso.value == 'pista');
-        comprarPista(qtd);
+    if(tipoIngresso.value == 'pista') {
+        comprarPista(quantidade);
+    } else if(tipoIngresso.value == 'superior') {
+        comprarSuperior(quantidade);
+    } else {
+        comprarInferior(quantidade);
+    }
+        
 
-
+    
 }
 
-function comprarPista(qtd) {
+function comprarPista(quantidade) {
     let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
-    if (qtd > qtdPista) {
+    if (quantidade > qtdPista) {
         alert('quantidade indisponivel para tipo pista ');
     } else {
-        qtdPista = qtdPista - qtd;
+        qtdPista = qtdPista - quantidade;
         document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
+} 
+
+function comprarSuperior(quantidade) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (quantidade > qtdSuperior) {
+        alert('quantidade indisponivel para tipo pista ');
+    } else {
+        qtdSuperior = qtdSuperior - quantidade;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+} 
+
+function comprarInferior(quantidade) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (quantidade > qtdInferior) {
+        alert('quantidade indisponivel para tipo pista ');
+    } else {
+        qtdInferior = qtdInferior - quantidade;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
         alert('Compra realizada com sucesso!');
     }
 } 
